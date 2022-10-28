ddb3a661d02f6d49212f20ef91cf48b5dc0fcb71
vikram <vikramkumar8655@gmail.com>
2022-10-29 01:42:55 +0530

    commit

--- social_book/settings.py
+++ social_book/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'social_book.urls'
@@ -123,6 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
+STATICFILES_STORAGE= 'whitenoise.storage.CompresseManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
